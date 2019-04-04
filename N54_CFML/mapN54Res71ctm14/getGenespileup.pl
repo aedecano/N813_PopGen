1fe75bae917baf3fc410ed0f5f110b54d5e3039b
Arun Decano <arundecano@gmail.com>
2019-04-04 17:29:55 +0100

    Create getGenespileup.pl

---
+++ N54_CFML/mapN54Res71ctm14/getGenespileup.pl
@@ -0,0 +1,24 @@
+system("ls *.pileup > pileup");
+open(PLUP, "pileup");
+@plup = <PLUP>;
+chomp(@plup);
+for $str (@plup){
+    open(IN, "$str");
+#    open(OUT, ">./Res71CTM14coverage/$str-coverage.txt");
+    #    print OUT "$str\n";
+    print OUT  "Position\tCoverage\n";
+    while(<IN>){
+        chomp;
+        @pileup = split/\t/,$_;
+        for $e (0..$#pileup){}
+        if ($pileup[1] >= 800 && $pileup[1] <= 3600 && $pileup[7] > 10){ #pctm14:874-1750
+#           print "$pileup[3]";
+            print OUT "$pileup[1]\t$pileup[7]\n"; #pCTM14 in Res7-1 coverage
+        }
+    }
+    #    print "\n";
+}
+
+close(IN);
+
+exit;
